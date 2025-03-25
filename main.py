9a549230f08a111fd97e6b356ae547f9862a610e
Vampire <rupalidhakate72@gmail.com>
2025-03-25 16:25:05 +0530

    Last Commit

---
+++ main.py
@@ -0,0 +1,17 @@
+
+from flask import Flask,render_template
+app = Flask(__name__)
+@app.route('/')
+def mainpage():
+    return render_template('index.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
+@app.route('/privacy')
+def privacy():
+    return render_template('privacy.html')
+@app.route('/usage')
+def usage():
+    return render_template('usage.html')
+if __name__ == '__main__':
+    app.run(debug=True,port=8000)
